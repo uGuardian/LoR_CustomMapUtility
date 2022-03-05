35eafb50f79411c03cfd5b5253ffba5ea3fbb122
uGuardian <Sean.Heavysege@gmail.com>
2022-03-05 16:03:39 -0700

    Created AbnoText Helper

--- SubComponents/CustomMapManager.cs
+++ SubComponents/CustomMapManager.cs
@@ -71,8 +71,10 @@ namespace CustomMapUtility {
 		string[] IBGM.GetCustomBGMs() => CustomBGMs;
 		/// <summary>
 		/// Override and specify a string array with audio file names (including extensions) for the get parameter.
-		/// If you put multiple strings in it'll change between them based on emotion level. (Emotion level 0, 2, and 4 respectively).
 		/// </summary>
+		/// <remarks>
+		/// If you put multiple strings in it'll change between them based on emotion level. (Emotion level 0, 2, and 4 respectively).
+		/// </remarks>
 		protected internal virtual string[] CustomBGMs {get;}
 		bool IBGM.AutoBGM {get => AutoBGM; set => AutoBGM = value;}
 		protected internal bool AutoBGM = false;
@@ -127,11 +129,57 @@ namespace CustomMapUtility {
 		string[] IBGM.GetCustomBGMs() => CustomBGMs;
 		/// <summary>
 		/// Override and specify a string array with audio file names (including extensions) for the get parameter.
-		/// If you put multiple strings in it'll change between them based on emotion level. (Emotion level 0, 2, and 4 respectively).
 		/// </summary>
+		/// <remarks>
+		/// If you put multiple strings in it'll change between them based on emotion level. (Emotion level 0, 2, and 4 respectively).
+		/// </remarks>
 		protected internal virtual string[] CustomBGMs {get;}
 		bool IBGM.AutoBGM {get => AutoBGM; set => AutoBGM = value;}
 		protected internal bool AutoBGM = false;
+
+		public override void CreateDialog() {
+			if (AbnoText != null) {
+				if (AbnoTextColor == null) {
+					_dlgEffect = SingletonBehavior<CreatureDlgManagerUI>.Instance.SetDlg(CreateDialogShared());
+				} else {
+					_dlgEffect = SingletonBehavior<CreatureDlgManagerUI>.Instance.SetDlg(CreateDialogShared(), AbnoTextColor);
+				}
+			} else {
+				IdxIterator(_creatureDlgIdList.Count);
+				if (AbnoTextColor == null) {
+					base.CreateDialog();
+				} else {
+					base.CreateDialog(AbnoTextColor);
+				}
+			}
+		}
+		public override void CreateDialog(Color txtColor) {
+			if (AbnoText != null) {
+				_dlgEffect = SingletonBehavior<CreatureDlgManagerUI>.Instance.SetDlg(CreateDialogShared(), AbnoTextColor);
+			} else {
+				IdxIterator(AbnoText.Count);
+				base.CreateDialog(txtColor);
+			}
+		}
+		protected internal virtual string CreateDialogShared() {
+			if (isEgo || AbnoText.Count <= 0) {return null;}
+			IdxIterator(AbnoText.Count);
+			if (_dlgIdx >= AbnoText.Count) {return null;}
+			if (_dlgEffect != null && _dlgEffect.gameObject != null) {
+				_dlgEffect.FadeOut();
+			}
+			return AbnoText[_dlgIdx];
+		}
+		protected internal virtual void IdxIterator(int totalEntries) {
+			if (!AbnoTextRandomOrder) {
+				_dlgIdx %= totalEntries;
+			} else {
+				_dlgIdx = UnityEngine.Random.Range(0, totalEntries);
+			}
+		}
+		protected internal virtual bool AbnoTextRandomOrder => false;
+		protected internal virtual List<string> AbnoText => null;
+		protected internal virtual Color AbnoTextColor {get;}
 	}
 	#endregion
 }

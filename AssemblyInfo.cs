cb933313ccbe5f4f5b9c6600a3d76bb9faa0e16a
uGuardian <Sean.Heavysege@gmail.com>
2022-05-16 14:05:19 -0600

    Added csproj and AssemblyInfo files

---
+++ AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+using System.Security.Permissions;
+using CustomMapUtility;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("uGuardian")]
+#if DEBUG
+#warning DEBUG
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+#else
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+#endif
+[assembly: System.Reflection.AssemblyFileVersionAttribute(CustomMapHandler.ModResources.CacheInit.version)]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute(CustomMapHandler.ModResources.CacheInit.version)]
+[assembly: System.Reflection.AssemblyProductAttribute("CustomMapUtility")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CustomMapUtility")]
+[assembly: System.Reflection.AssemblyVersionAttribute(CustomMapHandler.ModResources.CacheInit.version)]
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618

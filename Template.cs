32312139699b9ebe93a6d852e7cc2078d3a8bb5b
uGuardian <Sean.Heavysege@gmail.com>
2023-01-01 15:40:38 -0700

    changed mp3 to ogg for consistency.

--- Template.cs
+++ Template.cs
@@ -20,7 +20,7 @@ namespace Template {
         protected override string[] CustomBGMs {
             get {
                 // Put the file name of your BGM here, you don't need the full path.
-                return new string[] {"MyBGM.mp3"};
+                return new string[] {"MyBGM.ogg"};
             }
         }
     }
